79bc443afd05b903ef3668a355fb517f88468d1d
Kareth <kareth.marcela@gmail.com>
2015-10-28 16:10:41 -0500

    Update of header in the algorithm TwIST
    We need to check the "check's"

--- TwIST.cpp
+++ TwIST.cpp
@@ -1,52 +1,79 @@
 void TwIST(double *y,const object *A, double tau,double *&x,int arguments, ...){
 
-    int tmy = A->shots*A->apertures[0].m*(A->apertures[0].n+(A->l-1));
+    /*int tmy = A->shots*A->apertures[0].m*(A->apertures[0].n+(A->l-1));
     int tm = A->apertures[0].n*A->apertures[0].m*A->l;
     double f=1.0,tol_debias=0.001,num_nz_x=2.0,alpha=1.0;
     bool cont_debias_cg=true,keep_going;
     static double *RWpvec=0;
     double beta_cg=0.0,alpha_cg=0.0,rTr_cg_plus=0.0,rTr_cg=0.0,final_tau=0.0;
     double final_tolA=0.0001;
-    double tmp;
+    double tmp;*/
+    
+    int tm = A->apertures[0].n*A->apertures[0].m*A->l;
     //clock_t t0;
-    int iter=0;
-    int final_stopCriterion=4;
-    const unsigned Initial_X_supplied=80;
-    
-    
     int stopCriterion = 1;
     double tolA = 0.01;
-    bool debias = 0;
+    bool debias = false;
     int maxiter = 10000;
     int maxiter_debias = 200;
     int miniter = 5;
     int miniter_debias = 5;
     int init = 0;
-
-    
     int enforceMonotone = 1;
-    double alphamin = 1e-30;
-    double alphamax = 1e30;
     bool compute_mse = false;
+    int plot_ISNR = 0;
     bool verbose = true;
-    bool continuation = false;
-    int cont_steps = -1;
-    int firstTauFactorGiven = 0;
-    double firstTauFactor = 0.0,criterionObjective=0.0,realmin=0.0,lambda0=0.0;
-    double lambda=0.001,dGd=0.0,criterionActiveSet=0.0,delta_x_criterion=0.0;
-    double prev_f=0.0,dd=0.0,num_changes_active=0.0,criterionLCP=0.0;
+    double alpha = 0;
+    double beta = 0;
+    
+    int sparse = 1;
+    double tolD = 0.001
+    //variable phi_l1 and psi_ok type?
+    double phi_l1 = 0;
+    double psi_ok = 0;
+    //Default eigenvalues
+    double lam1 = 1e-4;
+    double lamN = 1;
+    //constants ans internal variables
+    double for_ever = 1;
+    //maj_max_sv: majorizer for the maximum singular value of operator A
+    int max_svd = 1;
+
+    //Set the defaults for outputs that may not be computed
+    int debias_start = 0;
+    double x_debias = [];
+    double mses = [];
+
     char *str=0;
     int i;
+
     
 
     va_list args;
     va_start(args, arguments);
-
+    
+             /*-----------------------
+                     Optional parameters
+            --------------------------*/
     while (true)
     {
-
         str = va_arg (args,char *);
-        if ( strcmp(str,"STOPCRITERION")==0){
+        if ( strcmp(str,"LAMBDA")==0){
+            lam1 = va_arg (args, double);
+            cout << "Lambda: "<<lam1 << '\n';            
+        }
+        else if (strcmp(str,"ALPHA")==0){
+            alpha = va_arg (args, double);
+            cout <<"Alpha: "<< alpha << '\n';
+        }
+        else if (strcmp(str,"BETA")==0){
+            beta = va_arg (args, double);
+            cout <<"Beta: "<< beta << '\n';
+        }
+        //-------
+        //Add Psi and Phi Varargin (Check dependencies) ----------------------------------------------------Check
+        //-------
+        else if ( strcmp(str,"STOPCRITERION")==0){
             stopCriterion = va_arg (args, int);
             cout << "Stop criterion: "<<stopCriterion << '\n';
         }
@@ -81,36 +108,24 @@ void TwIST(double *y,const object *A, double tau,double *&x,int arguments, ...){
         else if(strcmp(str,"INITIALIZATION")==0){
             init=va_arg (args, int);
             cout<<"Init: "<<init<<endl;
+            //Inizialization have an "If" ---> Check O_O ------------------------------------------------Check
         }
         else if(strcmp(str,"MONOTONE")==0){
             enforceMonotone = va_arg (args, int);
             cout <<"EnforceMonotone: "<< enforceMonotone << '\n';
         }
-        else if(strcmp(str,"CONTINUATION")==0){
-            continuation = va_arg (args, bool);
-            cout <<"Continuacion: "<< continuation << '\n';
-        }
-        else if(strcmp(str,"CONTINUATIONSTEPS")==0){
-            cont_steps = va_arg (args, int);
-            cout <<"ContSteps: "<< cont_steps << '\n';
-        }
-        else if(strcmp(str,"FIRSTTAUFACTOR")==0){
-            firstTauFactor = va_arg (args, int);
-            firstTauFactorGiven = 1;
-            cout <<"FirstTauFactor: "<< firstTauFactor << '\n';
+        else if(strcmp(str,"SPARSE")==0){
+            sparse = va_arg (args, int);
+            cout <<"Sparse: "<< sparse << '\n';
         }
         else if(strcmp(str,"TRUE_X")==0){
             compute_mse = true;
             va_arg (args, bool);
         }
-        else if(strcmp(str,"ALPHAMIN")==0){
-            alphamin = va_arg (args, double);
-            cout <<"Alpha min: "<< alphamin << '\n';
-        }
-        else if(strcmp(str,"ALPHAMAX")==0){
-            alphamax = va_arg (args, double);
-            cout <<"Alpha max: "<< alphamax << '\n';
-        }
+        else if(strcmp(str,"AT")==0){
+            AT = va_arg (args, int);
+            cout <<"AT: "<< AT << '\n';
+        }        
         else if(strcmp(str,"VERBOSE")==0){
             verbose = va_arg (args, int);
             cout <<"Verbose: "<< verbose << '\n';
@@ -120,4 +135,16 @@ void TwIST(double *y,const object *A, double tau,double *&x,int arguments, ...){
         }
     }
     va_end (args);
-
+    
+    //TwIST Parameters
+    double rho0;
+    rho0 = (1 - lam1/lamN)/(1 + lam1/lamN);
+    if (alpha == 0)
+    {
+        alpha = 2/(1+sqrt(1-rho0^2));
+    }
+    if (beta == 0)
+    {
+        beta  = (alpha*2)/(lam1+lamN);
+    }
+}
