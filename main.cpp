41f2cec1b5256c3d2101a13e20e30c2e605d5df7
karethkeon <kareth.marcela@gmail.com>
2015-11-27 10:44:42 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #define    SQH          0.707106781186547
 #define    SQRT_2       1.414213562373095
 #define    SHOTS        8
-#define    COLS         256
-#define    ROWS         256
+#define    COLS         4096
+#define    ROWS         1024
 #define    BANDS        24
 #define    MAXITERA     300
 #define    TAU          2e-5
@@ -24,6 +24,15 @@ typedef struct Result{
     float *psnr;
 }twist;
 
+typedef struct Matrix{ 
+    double *matrixA;
+    size_t m;
+    size_t n;
+}matriz;
+
+
+
+
 
 /*Función principal: 
 Recibe un vector de medidas de una imagen hiperespectral
@@ -35,28 +44,40 @@ int main()
     size_t shots = 1;
     size_t coded = 0;
     size_t count = 0;
+    int tmy = 1024;
+    int tm = 4096;
+    static float *y2= new float[tmy];
+    matriz *R = new matriz;
+    R ->m = ROWS;
+    R ->n = COL;
+    
     
     //Carga de las medidas
     ifstream myfile;
-    myfile.open ("medidas.txt");
+    myfile.open ("medidas_y_p1.txt");
     float output;
     int t1,t2,t3;
-/*
+
     if (myfile.is_open()){
-        while (count<(imagen->l*imagen->n*imagen->m)) {
-            myfile >> output;
-            t1 = count%imagen->m;
-            t2 = ((int)(count/imagen->n)) % imagen->n;
-            t3 = (int)(count/(imagen->n*imagen->m));
-            imagen->data[t1][t2][t3] = output;
-            count++;
+        for (int i=0; i < tmy ; i++) {
+			myfile >> output;
+			y2[i] = output;
+			//printf("This is the line = %10.3f\n", output);
         }
-    }*/
+    }
 
     myfile.close();
       
     //Llamada al algoritmo de reconstrucción
-    TwiST(y2,obj,tau,reconstruction,6,"TOLERANCEA",1e-12,"MAXITERA",MAXITERA,"VERBOSE",1,"FIN");
+    //TwiST(y2,obj,tau,reconstruction,6,"TOLERANCEA",1e-12,"MAXITERA",MAXITERA,"VERBOSE",1,"FIN");
+
+    /*FILE *pFile;
+
+    pFile = fopen ("../cassi/reconstruccion.txt","w");
+    for (i=0 ;i< tm*tmy ; i++){
+        fprintf (pFile, "%f\n",reconstruction[i]);
+    }
+    fclose (pFile);*/
 
     //delete[] imagen;
     return 0;
