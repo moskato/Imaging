a8093bb72b5712f5194506470feed284e1b7df27
moskat <oscarehc7@gmail.com>
2015-11-28 11:28:13 -0500

    creacion funcion vector_res y actualizacion header functions.h

---
+++ vector_res.cpp
@@ -0,0 +1,15 @@
+/*!
+ * Función vector_res:
+ * calcula la resta de dos vectores.
+ * 
+ * @param x puntero a vector de entrada.
+ * @param y puntero a vector de entrada.
+ * @param tm tamaño vectores de entrada
+ * @param z puntero a vector resultado de la resta de x y y.
+ * 
+ */
+void vector_res(float *x, float *y, int tm, float *z)
+{
+	for(int i=0;i<tm;i++)
+		z[i]=x[i]-y[i];
+}
