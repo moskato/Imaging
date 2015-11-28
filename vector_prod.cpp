3d600737ccba8aac6be0ed78fabb1f8dfda7d4eb
moskat <oscarehc7@gmail.com>
2015-11-28 11:15:52 -0500

    actualización y documentación funciones vector_prod y vector_sum

--- vector_prod.cpp
+++ vector_prod.cpp
@@ -2,11 +2,18 @@
  * Función vector_prod:
  * calcula el producto de dos vectores.
  * 
+ * @param x puntero a vector de entrada.
+ * @param y puntero a vector de entrada.
+ * @param tm tamaño vectores de entrada
+ * @return valor producto vectores de entrada.
+ * 
  */
 float vector_prod(float *x, float *y, int tm)
 {
-	float *z;
-	for(size_t i=0;i<tm;i++)
-		z=+x[i]*y[i];
+	float z=0;
+	for(int i=0;i<tm;i++)
+	{
+		z+=(x[i])*(y[i]);
+	}
 	return z;
 }
