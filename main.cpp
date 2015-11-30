251094464abc82639c538d72a6ce80ee0d99cadb
karethkeon <kareth.marcela@gmail.com>
2015-11-30 16:40:19 -0500

    Función Principal

--- main.cpp
+++ main.cpp
@@ -6,25 +6,32 @@ Recibe un vector de medidas de una imagen hiperespectral
 int main()
 {
     int tmy = 1024;
-    int tm = 256;
-    static float *reconstruction  = new float[tm];  
-    static float *y2= new float[tmy];
+    int tm = 4096;
+    double *reconstruction=new double[tm];  
+    double *y2=new double[tmy];
     static obj *R = new obj;
+    double *y_hat=new double[tmy];
+
     R ->m = ROWS;
     R ->n = COLS;     
     
     size_t i,j;
     
     //Crear Matrix R
-	R->matrixA = new float*[R->m];
+	R->matrixA = new double*[R->m];
+	double LO = -4;
+	double HI = 4;
 	
 	srand((unsigned)time(0));
-    for (i = 0; i < COLS; i++) 
+    for (i = 0; i < R->m; i++) 
     {
-		R->matrixA[i] = new float[R ->n];
-		for (j = 0; j < ROWS ; j++)
+		R->matrixA[i] = new double[R ->n];
+		for (j = 0; j < R->n ; j++)
 		{
-			R->matrixA[i][j]= rand()%2;
+			double out =  LO + static_cast <double> (rand()) /( static_cast <double> (RAND_MAX/(HI-LO)));
+			R->matrixA[i][j]= out;
+			//printf("This is the line = %10.3f\n", out);
+			
 
 		}
 	}
@@ -32,7 +39,7 @@ int main()
     //Carga de medidas observadas
     ifstream myfile;
     myfile.open ("medidas_y_p1.txt");
-    float output;
+    double output;
     if (myfile.is_open())
     {
         for (i=0; i < tmy ; i++) 
@@ -41,25 +48,93 @@ int main()
 			y2[i] = output;
 			//printf("This is the line = %10.3f\n", output);
         }
+        myfile.close(); 
     }
-    myfile.close();      
+         
+    
+    /*double c = phi_function(y2,tmy);
+    cout<<"y2= "<<c<<"\n"<<endl;
+    for (i = 0; i < tmy; i++)
+	{			
+				cout<<"y2= "<<y2[i]<<"\n"<<endl;
+	} */
     
     //Parámetros
     double tau = 20;
-	float tolA = 0.01;
+	double tolA = 0.01;
       
     //Llamada al algoritmo de reconstrucción
-    twist(y2,R->matrizA,tau,reconstruction,6,"TOLERANCEA",1e-12,"MAXITERA",MAXITERA);
+    twist(y2,R,tau,reconstruction,6,"TOLERANCEA",1e-12,"MAXITERA",MAXITERA,"FIN");
+	
+	/*Af(reconstruction, R, tm, tmy, y_hat); //Salida
+	double var;
+	/*
+	for(int i = 0; i < R->m ; i++)
+	{
+		var = 0;
+		for(int j = 0; j < R->n; j++)
+		{
+			var = var + R->matrixA[i][j]*reconstruction[j];
+			cout<<"M="<<R->matrixA[i][j]*reconstruction[j]<<"\n"<<endl;
+			//cout<<"var="<<var<<"\n"<<endl;
+		}
+		y_hat[i] = var;
+		//cout<<var<<"\n"<<endl;
+	}
+	
+	for(int i = 0; i < R->m ; i++)
+	{
+		for(int j = 0; j < R->n; j++)
+		{
+			cout<<R->matrixA[i][j]<<"\n"<<endl;
+		}
+	}*/
+	
+	/*
+	for (int i=0; i < tmy ; i++) 
+    {
+			//fprintf (fFile, "%f\n",y_hat[i]);
+			cout<<y_hat[i]<<"\n"<<endl;
+     }*/
+	
+	/*//Guardar salida;
+    FILE *fFile,
+    fFile = fopen ("../TwIST/y_hat.txt","w");
+    for (int i=0; i < tmy ; i++) 
+        {
+			fprintf (fFile, "%f\n",y_hat[i]);
+			printf("y_hat - y = %10.3f\n", y_hat[i] - y2[i]);
+        }
 
-    /*FILE *pFile;
-
-    pFile = fopen ("../cassi/reconstruccion.txt","w");
-    for (i=0 ;i< tm*tmy ; i++){
+    fclose (fFile);*/
+    
+    
+    ofstream mypfile;
+    mypfile.open ("../TwIST/reconstruccion.txt");
+    if (mypfile.is_open())
+    {
+        for (i=0; i < tm ; i++) 
+        {
+			mypfile<<reconstruction[i]<<endl;
+        }
+        mypfile.close(); 
+    }
+	/*
+    FILE *pFile;
+    pFile = fopen ("../TwIST/reconstruccion.txt","w");
+    for (i=0 ;i< tm ; i++){
         fprintf (pFile, "%f\n",reconstruction[i]);
     }
-    fclose (pFile);*/
-
+    fclose (pFile);
+*/
     //delete[] imagen;
+    delete[] reconstruction;
+    delete[] y2;
+    delete[] y_hat;
+    for (i = 0; i < R->m; i++) 
+    {
+		delete[] R->matrixA[i];
+	}
     return 0;
     
 }
